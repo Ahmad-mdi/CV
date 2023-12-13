4b1fea708f4d32ccc3bb3e56be5b9c73b545ef26
AhmadMdi <admimohammadi@gmail.com>
2023-12-14 01:27:32 +0330

    first router

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\home\HomeController;
+use App\Http\Controllers\panel\PanelController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//home:
+Route::get('/',[HomeController::class,'index']);
+Route::get('/adminPanel',[PanelController::class,'index']);
