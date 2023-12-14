fefa321858352357923aaeed224876b7ba30abc2
AhmadMdi <admimohammadi@gmail.com>
2023-12-15 01:31:14 +0330

    set RequestForm for authentications

---
+++ app/Http/Requests/auth/SignupRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'fullName' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|min:11|max:11|unique:users,phone',
+            'password' => 'required|min:6|max:20',
+        ];
+    }
+}
