85535440e6a64d3a91763f3aed850b9645f306b1
AhmadMdi <admimohammadi@gmail.com>
2023-12-15 02:31:11 +0330

    change file

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\home\HomeController;
 use App\Http\Controllers\panel\PanelController;
+use App\Http\Controllers\panel\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //home:
 Route::get('/',[HomeController::class,'index']);
-Route::get('/adminPanel',[PanelController::class,'index']);
+Route::delete('/logout',[AuthController::class,'logout']);
+
+//panel:
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('/',[PanelController::class,'index']);
+    Route::get('/profile',[ProfileController::class,'show']);
+    Route::get('/profile/edit',[ProfileController::class,'edit']);
+    Route::put('/profile/update',[ProfileController::class,'update']);
+});
+
+//auth:
+Route::middleware('guest')->group(function (){
+    Route::get('/login',[AuthController::class,'indexLogin']);
+    Route::post('/login',[AuthController::class,'login'])->name('login');
+    Route::get('/signup',[AuthController::class,'indexSignup']);
+    Route::post('/signup',[AuthController::class,'signup']);
+});
